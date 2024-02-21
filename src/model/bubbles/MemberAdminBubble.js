9d1459800778d8a386ad254a1e0d23139d4ff04c
Dave.Potter <dave@datonalabs.org>
2024-02-21 13:27:04 +0000

    save shared admin member bubble state to bubble to prevent the need for all member files to read on startup

--- src/model/bubbles/MemberAdminBubble.js
+++ src/model/bubbles/MemberAdminBubble.js
@@ -1,4 +1,4 @@
-import { Bubble, EncryptionPolicy, assert, bubbleProviders, encryptionPolicies, toDelegateSignFunction, toFileId, userManagers } from "@bubble-protocol/client";
+import { Bubble, BubbleFilename, EncryptionPolicy, assert, bubbleProviders, encryptionPolicies, toDelegateSignFunction, toFileId, userManagers } from "@bubble-protocol/client";
 import { ecdsa } from "@bubble-protocol/crypto";
 import { Key, publicKeyToAddress } from "@bubble-protocol/crypto/src/ecdsa";
 import { hexToUint8Array, uint8ArrayToHex } from "@bubble-protocol/crypto/src/utils";
@@ -10,6 +10,8 @@ import { stateManager } from "../../state-context";
 export class MemberAdminBubble {
 
   bubble;
+  members = [];
+  mtime = 0;
 
   constructor(config, account, loginKey, delegation) {
     assert.isObject(config, 'config');
@@ -36,7 +38,7 @@ export class MemberAdminBubble {
     console.trace('initialising member admin bubble', this.bubble.contentId, this.dataFile);
     await this.bubble.initialise()
       .catch(error => {
-        console.debug(error)
+        console.warn(error)
         if (error.message.match(/^user metadata file is missing/)) throw new AppError('Your administrator user file is missing from the bubble. Send the following code to the community administrator: "'+this.loginKey.cPublicKey+'"', {code: 'missing-admin-file', publicKey: this.loginKey.cPublicKey});
         else throw error;
       })
@@ -45,29 +47,36 @@ export class MemberAdminBubble {
 
   async addAdminMember(account, publicKey) {
     console.trace('adding member with public key:', publicKey)
-    const address = publicKeyToAddress(publicKey);
     await this.bubble.userManager.addUser({publicKey, metadataFile: MEMBER_ADMIN_DIR+'/'+account});
   }
 
   async refreshAllMembers() {
-    const files = await this.bubble.list(toFileId(0));
+    await this._loadState();
+    const files = await this.bubble.list(toFileId(0), {modified: true});
     const memberFiles = files.filter(f => f.name.slice(0,3) !== '0x8');
-    const promises = memberFiles.map(f => {
-      const account = '0x' + f.name.slice(26);
+    const existingMembers = this.members.filter(member => memberFiles.findIndex(file => member.file.name === file.name) >= 0);
+    const newMembers = memberFiles.filter(file => this.members.findIndex(member => member.file.name === file.name) < 0).map(f => { return {file: f, account: '0x' + f.name.slice(26)}});
+    this.members = [...existingMembers, ...newMembers];
+    const newOrUpdatedFiles = memberFiles.filter(file => file.modified > this.mtime);
+    this.mtime = newOrUpdatedFiles.reduce((mtime, file) => file.modified > mtime ? file.modified : mtime, this.mtime);
+    const promises = newOrUpdatedFiles.map(f => {
+      const member = this.members.find(member => member.file.name === f.name);
+      console.trace('reading file', f.name, member)
       return this.bubble.read(f.name)
       .then(json => { 
         const socials = JSON.parse(json);
-        return {account, ...socials}
+        Object.assign(member, socials);
       })
       .catch(error => {
         console.warn('Failed to read member file', f.name);
         console.warn(error);
-        return {account};
       });
     });
-    this.members = await Promise.all(promises);
-    console.debug('members', this.members);
+    await Promise.all(promises);
+    this.members.sort((a,b) => a.account.localeCompare(b.account));
+    console.trace('members', this.members);
     stateManager.dispatch('all-members', this.members);
+    if (promises.length > 0) await this._saveState();
   }
 
   deleteMemberFile(account) {
@@ -78,6 +87,29 @@ export class MemberAdminBubble {
     })
   }
 
+  /**
+   * @dev Loads the admin bubble state from localStorage
+   */
+  async _loadState() {
+    const stateJSON = await this.bubble.read(toFileId(MEMBER_ADMIN_DIR, 'state'), {silent: true});
+    const stateData = stateJSON ? JSON.parse(stateJSON) : {};
+    console.trace('loaded admin bubble state', stateData);
+    this.mtime = stateData.mtime || 0;
+    this.members = stateData.members || [];
+  }
+
+  /**
+   * @dev Saves the admin bubble state to localStorage
+   */
+  async _saveState() {
+    console.trace('saving admin bubble state');
+    const stateData = {
+      mtime: this.mtime,
+      members: this.members
+    };
+    await this.bubble.write(toFileId(MEMBER_ADMIN_DIR, 'state'), JSON.stringify(stateData));
+  }
+    
 }
 
 
@@ -121,7 +153,13 @@ class MemberAdminEncryptionPolicy extends encryptionPolicies.MultiEncryptionPoli
 class MemberAdminDirEncryptionPolicy extends encryptionPolicies.AESGCMEncryptionPolicy {
 
   isEncrypted(contentId) {
-    return contentId.file === MEMBER_ADMIN_DIR;
+    const filename = new BubbleFilename(contentId.file || contentId);
+    return filename.isValid() && filename.getPermissionedPart() === MEMBER_ADMIN_DIR;
+  }
+
+  decrypt(data) {
+    if (!data) return Promise.resolve('');
+    return super.decrypt(data);
   }
 
 }
@@ -145,10 +183,9 @@ class MemberDataEncryptionPolicy extends EncryptionPolicy {
   }
 
   decrypt(json) {
-    console.debug('decrypting', json)
+    if (!json) return Promise.resolve('');
     const data = JSON.parse(json);
     const sharedSecret = uint8ArrayToHex(secp256k1.ecdh(hexToUint8Array(data.publicKey), this.adminKey));
-    console.debug('shared secret:', sharedSecret)
     const decryptor = new encryptionPolicies.AESGCMEncryptionPolicy(sharedSecret);
     return decryptor.decrypt(data.data).then(buf => Buffer.from(buf).toString());
   }
